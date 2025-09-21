e9dc3d4c09c84fb49cdbcf2ee69cd47c921d293a
Ayush Garg <ayush@Ayushs-MacBook-Pro.local>
2025-09-21 13:59:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useLocation } from "react-router-dom";
+import Sidebar from './Sidebar/Sidebar';
+import TopHeader from './TopHeader/TopHeader';
+import TopHeader2 from './TopHeader2/TopHeader2';
+import DashboardKPI from './DashboardKPI/DashboardKPI';
+import RevenueSection from './RevenueSection/RevenueSection';
+import ProductsSales from './ProductsSales/ProductsSales';
+import RightSidebar from './RightSidebar/RightSidebar';
+import OrderList from './OrderList/OrderList';
 
 function App() {
+  const location = useLocation();
+  const isOrdersPage = location.pathname === "/orders";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Sidebar />
+
+      {isOrdersPage ? (
+        <div
+          className="orders-wrapper"
+          style={{ marginLeft: "262px", marginTop: "14px", width: "79%"}} // example styles for orders page
         >
-          Learn React
-        </a>
-      </header>
+          <Routes>
+            <Route
+              path="/orders"
+              element={
+                <>
+                  <TopHeader2 />
+                  <OrderList />
+                </>
+              }
+            />
+          </Routes>
+        </div>
+      ) : (
+        <div
+          className="main-content"
+          style={{ marginLeft: "256px", marginTop: "64px", marginRight: "320px" }}
+        >
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <TopHeader />
+                  <DashboardKPI />
+                  <RevenueSection />
+                  <ProductsSales />
+                  <RightSidebar />
+                </>
+              }
+            />
+          </Routes>
+        </div>
+      )}
     </div>
   );
 }
